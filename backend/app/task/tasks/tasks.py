115678361803bde399d6a4a8ea3c5c77001e2486
guhua <819070918@qq.com>
2025-10-28 19:45:39 +0800

    refactory

--- backend/app/task/tasks/tasks.py
+++ backend/app/task/tasks/tasks.py
@@ -2,16 +2,19 @@ from time import sleep
 
 from sqlalchemy import text
 from anyio import sleep as asleep
+from celery.utils.log import get_logger
 
 from backend.app.task.celery import celery_app
 from backend.app.task.utils.email_util import email_service
-from backend.common.log import log
 from backend.database.db import async_db_session
+
+logger = get_logger('mes.tasks')
 
 
 @celery_app.task(name='task_demo')
 def task_demo() -> str:
     """示例任务，模拟耗时操作"""
+    logger.info("开始同步示例任务")
     sleep(30)
     return 'test async'
 
@@ -19,6 +22,7 @@ def task_demo() -> str:
 @celery_app.task(name='task_demo_async')
 async def task_demo_async() -> str:
     """异步示例任务，模拟耗时操作"""
+    logger.info("开始异步示例任务")
     await asleep(30)
     return 'test async'
 
@@ -26,18 +30,19 @@ async def task_demo_async() -> str:
 @celery_app.task(name='task_demo_params')
 async def task_demo_params(hello: str, world: str | None = None) -> str:
     """参数示例任务，模拟传参操作"""
+    logger.info("开始参数示例任务")
     await asleep(1)
     return hello + world
 
 
 @celery_app.task(name="task_email_send")
-async def task_email_send():
+async def task_email_send() -> str:
     """执行存储过程并发送邮件通知"""
-    await _task_email_send_async()
+    return await _task_email_send_async()
 
 
 async def _task_email_send_async():
-    log.info("📨 [task_email_send] 开始执行存储过程并准备发送邮件")
+    logger.info("📨 [task_email_send] 开始执行存储过程并准备发送邮件")
 
     try:
         async with async_db_session.begin() as session:
@@ -56,7 +61,9 @@ async def _task_email_send_async():
                 body=f"MES Monitor ISC精细化管理接口上报预警 - {ret_msg}"
             )
 
-        log.info(f"✅ [task_email_send] 执行成功，结果：{ret_msg}")
+        logger.info(f"✅ [task_email_send] 执行成功，结果：{ret_msg}")
+
+        return ret_msg
 
     except Exception as e:
-        log.error(f"❌ [task_email_send] 任务执行失败: {e}", exc_info=True)
+        logger.error(f"❌ [task_email_send] 任务执行失败: {e}", exc_info=True)
